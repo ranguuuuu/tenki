6113ed80f1326779edb2503fb5f5812c82e7b81f
ranguuuuu <ra.menso.men0723@gmail.com>
2023-01-11 22:46:51 +0900

    first commit.

---
+++ src/main/java/com/early2home/TenkiApplication.java
@@ -0,0 +1,13 @@
+package com.early2home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/early2home/TenkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.early2home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
